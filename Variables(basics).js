8f1946621c16659f01e2c351e2766de97b0fff05
niranjan-2024 <89894972+niranjan-2024@users.noreply.github.com>
2022-07-16 11:16:10 +0530

    Create Variables(basics).js

---
+++ Variables(basics).js
@@ -0,0 +1,43 @@
+var a = 57;
+console.log(a);
+a = "niranjan"
+console.log(a)
+console.log("\n")
+
+let b = 55;
+console.log(b);
+b = "niranjan"
+console.log(b)
+
+var $ = 45
+console.log($)
+
+var $1 = 4500000000000000
+console.log($1)
+
+var _ = "Apple is \n a Fruit"
+console.log(_)
+
+console.log(a,'\n',b,'\n',_)
+
+var __ = "Apple is \t a Fruit"
+console.log(__)
+
+//var 1a = 12 -- Error
+//let var = mango
+
+
+/*output
+57
+niranjan
+55
+niranjan
+45
+4500000000000000
+Apple is 
+ a Fruit
+niranjan 
+ niranjan 
+ Apple is 
+ a Fruit
+Apple is 	 a Fruit*/
